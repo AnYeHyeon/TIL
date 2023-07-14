82991e4b2044428e7c83eaeb68ede96ee9e089d0
AnYeHyeon <ahnyh4756@gmail.com>
2023-07-14 21:26:40 +0900

    Update jsStars.js

--- jsStars.js
+++ jsStars.js
@@ -1,8 +1,17 @@
-function jsStars(number) {
-    for (i = 1; i<number; i++) {
-      console.log('*')
-      i = 2*i + 1 
+// function jsStars(number) {
+//     for (i = 0; i<number; i++) {
+//       console.log('*'.repeat(2*i + 1))
+//   }
+// }
+// number = prompt('숫자를 입력해주세요.');
+// jsStars(number);
+
+for (i = 0; i < 10; i++) {
+    for (j = 1; j < 10 - i; j++) {
+        console.log('&nbsp');
     }
-  }
-  number = prompt('숫자를 입력해주세요.')
-  jsStars(number)
+    for (j = 0; j < i * 2 + 1; j++) {
+        console.log("*");
+    }
+    console.log("<br>");
+}
